98701e0cde2976328ad37e6650de90191d029d6d
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-08-03 22:38:59 +0300

    Has done markup

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li key={id}>
+          <span>{isOnline}</span>
+          <img src={avatar} alt={name} width="48" />
+          <p>{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
